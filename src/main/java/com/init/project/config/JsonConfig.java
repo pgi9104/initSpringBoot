7e4e9c1c0b430f5df6353d090c1fc53333507fac
park91s <park91s@naver.com>
2024-08-05 21:17:55 +0900

    [Json 설정]

---
+++ src/main/java/com/init/project/config/JsonConfig.java
@@ -0,0 +1,13 @@
+package com.init.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+public class JsonConfig {
+	@Bean
+	MappingJackson2JsonView jsonView() {
+		return new MappingJackson2JsonView();
+	}
+}
